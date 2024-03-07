b67bdb41d281854187cf57754f0aea4848c52ff9
Karolina Redden <dubajkaro@gmail.com>
2024-03-07 16:13:30 -0400

    add a unique restaurant id backend route

---
+++ fct-project-main/src/app/yelp/[id]/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer O3r0O9LGi-Ug-FUWsrT9VwV7PwZndrrhJnzJrjmqNFds2ZpoAnuU_iO5v-DwCTxqoN2HXgbHF-FFIYrF_baUfJJjch8gsXqxDuXqo7FM31NKv2hululT0gfPwf3dZXYx'
+  }
+};
+
+interface Restaurant {
+  name: string;
+  id: string;
+}
+
+export async function GET(request: Request) {
+  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+  console.log(id)
+  const res = await fetch(`https://api.yelp.com/v3/businesses/${id}`, options);
+  const restaurant: Restaurant = await res.json();
+  if (!restaurant.id) return NextResponse.json({ message: "Inavlid" })
+  return NextResponse.json(id);
+}
+

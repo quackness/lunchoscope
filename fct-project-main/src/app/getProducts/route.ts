19f869ec774b86ac982a11169a1242290b105dde
Karolina Redden <dubajkaro@gmail.com>
2024-03-19 12:54:44 -0300

    get prices endpoint

--- fct-project-main/src/app/getProducts/route.ts
+++ fct-project-main/src/app/getProducts/route.ts
@@ -3,6 +3,17 @@ import { NextResponse } from 'next/server';
 
 // https://www.youtube.com/watch?v=d4sMYnenaU8
 
+
+// const apiKey = 
+// console.log(`apiKey: ${apiKey}`);
 export async function GET(request: any) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: null
+  });
+
+  const prices = await stripe.prices.list({
+    limit: 4
+  })
+  return NextResponse.json(prices.data)
 
 }

2b05a4fbc725805dfb1fdecc04efaf7d0defefaf
Karolina Redden <dubajkaro@gmail.com>
2024-03-19 16:29:44 -0300

    add payment processor

---
+++ fct-project-main/src/app/payment/route.tsx
@@ -0,0 +1,29 @@
+import Stripe from 'stripe';
+import { NextResponse, NextRequest } from 'next/server';
+
+
+export async function POST(request: any) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: null
+  });
+  console.log(">>", stripe)
+  let data = await request.json();
+  let priceId = data.priceId
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+        {
+            price: priceId,
+            quantity: 1
+        }
+    ],
+  mode: 'subscription',
+  //change those for production
+  success_url: 'http://localhost:3000',
+  cancel_url: 'http://localhost:3000'
+})
+
+return NextResponse.json(session.url)
+}
+
+
+//custom fields https://www.youtube.com/watch?v=d4sMYnenaU8
--- fct-project-main/src/components/Frontend/pricingCard.tsx
+++ fct-project-main/src/components/Frontend/pricingCard.tsx
@@ -1,24 +1,81 @@
 import axios from 'axios';
-import Link from "next/link";
 import {AiFillCheckCircle} from 'react-icons/ai';
+
+//https://github.com/bwestwood11/stripe-checkout-nextjs13/blob/main/app/components/PricingCard.jsx
 
 const PricingCard = ({price}) => {
 
     const dynamicSubTitle = (price) => {
       if (price.nickname === "FREE Trial") {
-        return <p className="text-[#f1592a] mt-1">5 Sentiment analysis per month</p>;
+        // return <p className="text-[#f1592a] mt-1">5 Sentiment analysis per month</p>;
+        return (
+          <div className="mt-6 space-y-4">
+            <div className="flex space-x-3">
+          <AiFillCheckCircle
+            className="h-5 w-5 flex-shrink-0 text-green-500 ml-2"
+            aria-hidden="true"
+          />
+          <p className="text-sm text-gray-500">5 Sentiment analysis per month</p>
+        </div>
+        </div>
+        )
       } else if (price.nickname === "Lunchoscope Annual Subscription") {
         return (
-          <div>
-        <p className="text-[#f1592a] mt-1">Unlimited Sentiment Analysis</p>
-        <p className="text-[#f1592a] mt-1">20% discount</p>
-        <p className="text-[#f1592a] mt-1">24/7 Support</p>
-        </div>
+          <>
+          <div className="mt-6 space-y-4">
+          <div className="flex space-x-3">
+        <AiFillCheckCircle
+          className="h-5 w-5 flex-shrink-0 text-green-500 ml-2"
+          aria-hidden="true"
+        />
+        <p className="text-sm text-gray-500">Unlimited Sentiment Analysis</p>
+      </div>
+      <div className="border" />
+      </div>
+          <div className="mt-6 space-y-4">
+          <div className="flex space-x-3">
+        <AiFillCheckCircle
+          className="h-5 w-5 flex-shrink-0 text-green-500 ml-2"
+          aria-hidden="true"
+        />
+        <p className="text-sm text-gray-500">20% discount</p>
+      </div>
+      </div>
+     </>
+
         );
       } else if (price.nickname === "Lunchoscope Monhtly Subscription") {
-        return <p className="text-[#f1592a] mt-1">Unlimited Sentiment Analysis</p>;
+        return (
+          <div className="mt-6 space-y-4">
+          <div className="flex space-x-3">
+        <AiFillCheckCircle
+          className="h-5 w-5 flex-shrink-0 text-green-500 ml-2"
+          aria-hidden="true"
+        />
+        <p className="text-sm text-gray-500">Unlimited Sentiment Analysis</p>
+      </div>
+      </div>
+        );
       }
     }
+
+// POST request 
+const handleSubscription = async (e: any) => {
+  e.preventDefault();
+  const { data } = await axios.post('http://localhost:3000/payment',
+  {
+    priceId: price.id
+  },
+  {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+  );
+  console.log("data", data)
+  // window.location.assign(data)
+}
+
   
   return (
     <div className="border-grey-100 shadow-2xl border-4 text-center mt-10 max-w-[1040px]">
@@ -32,17 +89,13 @@ const PricingCard = ({price}) => {
               {(price.unit_amount / 100).toLocaleString('en-CA',{ style: 'currency', currency: 'CAD'})}</h1>
             </div>
           </div>
-          <button className="mt-8 flex w-full justify-center rounded-md border border-transparent bg-[#f1592a] py-2 px-4 text-sm font-medium text-white shadow-sm" onClick={()=> {}}>
+          <button className="mt-8 flex w-full justify-center rounded-md border border-transparent bg-[#f1592a] py-2 px-4 text-sm font-medium text-white shadow-sm" onClick={handleSubscription}>
              Subscribe
           </button>
-
         </div>
       </div>
-
     </div>
-
   )
-
 }
 
 export default PricingCard

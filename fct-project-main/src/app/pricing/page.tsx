1ea80268ec13d97211267ff54df89d37301759d2
Zhengyq987 <yqzheng6@gmail.com>
2024-04-05 11:53:47 -0300

    pricing page

--- fct-project-main/src/app/pricing/page.tsx
+++ fct-project-main/src/app/pricing/page.tsx
@@ -2,6 +2,7 @@
 import PricingCard from '@/components/Frontend/pricingCard';
 import axios from 'axios';
 import { useState, useEffect} from 'react';
+import Layout from '@/components/Frontend/layout';
 
 export interface Price {
   id: string;
@@ -52,18 +53,20 @@ const Pricing = () => {
     setPrices(getPrices)
   }
   return (
+    <Layout>
 <section className="w-full">
          <div className="mx-auto max-w-4xl text-center mt-10 items-center">
               {/* <h2 className="text-3xl font-semibold leading-7 text-[#f1592a]">Exclusive Member Pricing</h2> */}
               <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Exclusive Member Pricing</h1>
               <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600 sm:text-center">Sign up today and never wonder where to eat out again</p>
          </div>
-         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1040px] items-center mx-auto">
+         <div className="grid grid-cols-1 sm:grid-cols-3 mb-12 gap-8 max-w-[1040px] items-center mx-auto">
            {prices && prices.map(price => (
            <PricingCard price={price} key={price.id}/>
            ))}
          </div>
    </section>
+   </Layout>
   )
 }
 
